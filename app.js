22483a01ac3b8895d51e41dd3f94d22bc65bbdac
logalpinist <83462138+logalpinist@users.noreply.github.com>
2026-06-07 18:23:20 +0900

    Update app.js

--- app.js
+++ app.js
@@ -3,14 +3,23 @@ function startSensor() {
     window.addEventListener("deviceorientation", (event) => {
 
         let roll = Math.round(event.gamma);
+        let pitch = Math.round(event.beta);
 
         document.getElementById("rollBoat")
             .style.transform =
             `rotate(${roll}deg)`;
 
+        document.getElementById("pitchBoat")
+            .style.transform =
+            `rotate(${pitch}deg)`;
+
         document.getElementById("rollValue")
             .innerText =
             `${roll}°`;
+
+        document.getElementById("pitchValue")
+            .innerText =
+            `${pitch}°`;
 
     });
 
